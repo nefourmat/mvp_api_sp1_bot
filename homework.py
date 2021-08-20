1d33bc45691c1ccb130f4e4010e116d9faa0e8c5
Михаил <nefourmat0@gmail.com>
2021-08-20 20:57:12 +0200

    first commit

--- homework.py
+++ homework.py
@@ -1,25 +1,28 @@
 import os
 import time
+import logging
 import requests
-import telegram
-from dotenv import load_dotenv
 
+from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
+from telegram import Bot
 
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TIME_UNIX = 0
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+# проинициализация бота
+bot = Bot(os.getenv('TELEGRAM_TOKEN'))
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    if homework_status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,12 +30,16 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    data = {
+        'from_date': current_timestamp
+    }
+    homework_statuses = requests.get(url=URL, headers=headers, params=data)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -40,12 +47,25 @@ def main():
 
     while True:
         try:
-            ...
+            new_homework = get_homeworks(current_timestamp)
+            if new_homework.get('homeworks'):
+                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date')
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log', 
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('my_logger.log', maxBytes=50000000, backupCount=5)
+logger.addHandler(handler)
 
 
 if __name__ == '__main__':
